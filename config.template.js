62ec5b0f07c11ada9c6455582a8e53edebc56e65
thtimo <timo.hintsa@vtt.fi>
2014-06-13 09:59:42 +0300

    tried out instagram too

--- config.template.js
+++ config.template.js
@@ -8,6 +8,9 @@ config.twitter.api_secret = '...';
 config.twitter.access_token = '...';
 config.twitter.access_token_secret = '...';
 
+config.instagram.client_id = '...';
+config.instagram.client_secret = '...';
+
 config.web.port = process.env.WEB_PORT || 3000;
 config.web.basepath = '';
 
--- routes/index.js
+++ routes/index.js
@@ -4,24 +4,39 @@ var twit = require('twit');
 var config = require('../config');
 
 var t = new twit({
-    consumer_key: config.twitter.api_key
-  , consumer_secret: config.twitter.api_secret
-  , access_token: config.twitter.access_token
-  , access_token_secret: config.twitter.access_token_secret
-})
+		consumer_key : config.twitter.api_key,
+		consumer_secret : config.twitter.api_secret,
+		access_token : config.twitter.access_token,
+		access_token_secret : config.twitter.access_token_secret
+	});
 
+var ig = require('instagram-node').instagram();
+ig.use({
+	client_id : config.instagram.client_id,
+	client_secret : config.instagram.client_secret
+});
 
 /* GET home page. */
-router.get(config.web.basepath + '/', function(req, res) {
-	t.get('statuses/user_timeline', { screen_name: 'boodlare', count: 10 }, function(err, data, response) {
-//	t.get('search/tweets', { screen_name: 'boodla since:2011-11-11', count: 100 }, function(err, data, response) {
-		console.log(data);
-		var items = Array();
-		//data.statuses.forEach(function(item) {
-		data.forEach(function(item) {
-			items.push(item.text + "\n\n");
+router.get(config.web.basepath + '/', function (req, res) {
+	t.get('search/tweets', {
+		q : 'urbangardening since:2011-11-11',
+		count : 10
+	}, function (err, data, response) {
+		console.log(response);
+		var items = data;
+
+		ig.tag_media_recent('urbangardening', function (err, medias, pagination, limit) {
+			//console.log(medias);
+
+			var grams = medias;
+
+			res.render('index', {
+				title : 'Express',
+				tweets : JSON.stringify(items),
+				instagrams : JSON.stringify(grams)
+			});
 		});
-		res.render('index', { title: 'Express', tweets: items });
+
 	})
 });
 
